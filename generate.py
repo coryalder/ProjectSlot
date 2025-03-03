6e4d918d5df18f07a0e9d6bc5b452b667ed50f24
Cory <hello@crowselectromusic.com>
2025-03-02 17:59:25 -0800

    working generate script locally

--- generate.py
+++ generate.py
@@ -5,10 +5,18 @@ import Mesh
 doc = FreeCAD.open('projectslot.FCStd')
 
 sheet = doc.Spreadsheet
-#sheet.set("radius", str(123))
+#sheet.set("pcb_width", str(20))
+#sheet.set("rail_length", str(120))
+#sheet.set("chamfer", str(6))
 sheet.recompute()
 
-for objName in ["projectslot", "ps-pcb-mount", "pcb-slide-in-clip"]:
-    obj = doc.getObject(objName);
-    obj.touch();
-    Mesh.export([obj], f"{objName}.stl")
+targetObjLabels = ["projectslot", "ps-pcb-mount", "pcb-slide-in-clip"];
+
+for obj in doc.Objects:
+    print(obj.Label);
+    if obj.Label in targetObjLabels:
+        obj.touch();
+        print(f"exporting {obj.Label}")
+        Mesh.export([obj], f"stls/{obj.Label}.stl")
+    else:
+        print(f"skipping {obj.Label}")
