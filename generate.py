56fccd9a60b63fe29475e930e4e34f4a0d7b91ee
Cory <hello@crowselectromusic.com>
2025-03-03 15:59:15 -0800

    extract and use appimage

--- generate.py
+++ generate.py
@@ -1,3 +1,22 @@
+import sys
+import os
+
+freecad_paths = [
+    '/home/runner/work/lumenpnp/lumenpnp/squashfs-root/usr/lib',  # For CI when using AppImage
+    '/mnt/c/Users/computer/Code/ProjectSlot/squashfs-root/usr/lib', # for my machine
+    '/usr/lib/freecad/lib/',  # For CI
+    '/usr/lib/freecad-daily-python3/lib/',  # For Ubuntu
+    '/usr/lib64/freecad/lib64/',  # For Fedora
+    '/Applications/FreeCAD.app/Contents/MacOS',  # For Mac OS X
+    'c:/Program Files/FreeCAD 0.18/bin/',  # For Windows
+    'c:/Program Files/FreeCAD 0.19/bin/',  # For Windows
+]
+
+for path in freecad_paths:
+    if os.path.exists(path):
+        print(f"Added possible FreeCAD path: {path}")
+        sys.path.append(path)
+
 import FreeCAD
 import Mesh
 
